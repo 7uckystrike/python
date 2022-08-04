ee0e938e6cc5164d45a30471223d4c5772f54899
yeoung <yeoung.dev@gmail.com>
2022-08-04 22:11:39 +0900

    가위바위보 게임

--- 가위바위보/가위바위보.py
+++ 가위바위보/가위바위보.py
@@ -1,5 +1,34 @@
-print ('가위바위보 게임을 시작하겠습니다!')
+import random
 
-answer = input('입력하세요!')
+print ('가위바위보 게임을 시작합니다!')
 
+game = ['가위', '바위', '보']
+
+computer = random.choice(game)
+answer = input('무엇을 낼까?')
+
+
+print ('컴퓨터:' + computer)
 print ('나:' + answer)
+
+if computer == '가위' :
+  if answer == '가위' :
+    print('비겼다!')
+  elif answer == '보' :
+    print('졌당!!')
+  elif answer == '주먹' :
+    print('이겼당!!!!')
+elif computer == '보' :
+  if answer == '보' :
+    print('비겼습니다요..')
+  elif answer == '가위' :
+    print('졌습니다..')
+  elif answer == '주먹' :
+    print('이겼다요!!!!')
+elif computer == '주먹' :
+  if answer == '보' :
+    print('졌습니다.....') 
+  elif answer == '주먹' :
+    print('비겼습니다다아아아!!!')
+  elif answer == '가위' :
+    print ('이겼습니다아아!!')
